c2f46d866e63314b21437ada4dd16b9142d8ef0d
comnicgithub <nicolas@comnic.com.ar>
2023-04-07 12:05:29 -0300

    Entrega N°1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # backend-39770
-probando rama
+» Se creo un constructor de objetos llamado 'ProductManager'.
+
+» Dentro del mismo se definio un array vacio ('this.products'), tambien se agrego el precio del iva a sumar al valor del producto.
+
+» Se definio la variable 'product' como nombre para cada producto nuevo que se cree.
+
+» Se creo dentro de'ProductManager' un metodo 'getProduct' para traer todos los productos que hay en stock con todas sus propiedades.
+
+» Se creo dentro de'ProductManager' un metodo 'addProduct' el cual permite agregar nuevos productos al array 'this.products', este metodo permite agregar las siguientes propiedades de forma manual; title, description, price, thumbnail, stock; También de creo un condicional 'If' para configurar el id del producto cargado y que se autoincremente. Se configuro el calculo sobre el precio para sumarle el iva. Definimos la variable 'product' y le indicamos que vayan todos los valores que necesitamos 'id,title, description, price, thumbnail, stock'. Por ultimo usamos el metodo 'push' para subir el producto al array 'this.products'.
+
+» Se creo dentro de'ProductManager' un metodo 'getProductById', el cual permite ubicar dentro del array 'this.products' el producto por su numero de 'id' mediante un 'find' que recorra todo el array y devuelva el 'id' requerido, en caso contrario se configuro un clg con un string que diga not found y devuelve un null.
+
+» Se probaron los metodos 'addProduct' ingresando 6 productos nuevos, 'getProducts' trayendo todo el array de los productos pusheados y 'getProductById', este ultmimo poniendo un valor true y un valor null para confirmar su correcto funcnionamiento.
+
--- app.js
+++ app.js
@@ -1,143 +1,77 @@
-//Definir la clase TicketManager, el cual tendrá un arreglo de eventos que iniciará vacío
-//La clase debe contar con una variable privada “gain”, que será la ganancia de un ticket (15%)
-//Debe contar con el método “getEvents” que mostrará los eventos guardados.
-/* 
-Debe contar con el método “addEvent” que recibirá los siguientes parámetros:
-name
-place
-price (al cual hay que agregarle la ganancia adicional)
-capacity (si no se da: 50 por defecto)
-date (si no se da: hoy por defecto)
-El método deberá crear además el campo id autoincrementable y el campo “participants” que siempre iniciará con un arreglo vacío.
-*/
+//Realizar una clase “ProductManager” que gestione un conjunto de productos.
+//Debe crearse desde su constructor con el elemento products, el cual será un arreglo vacío.
+
+//Cada producto que gestione debe contar con las propiedades:
+// title (nombre del producto)
+// description (descripción del producto)
+// price (precio)
+// thumbnail (ruta de imagen)
+// id (código identificador)
+// stock (número de piezas disponibles)
+
+// Aspectos a incluir
+// Debe contar con un método “addProduct” el cual agregará un producto al arreglo de productos inicial.
+// Todos los campos son obligatorios menos id que debe agregarse automáticamente  y auto- incrementable
+
+// Debe contar con un método “getProducts” el cual debe devolver el arreglo con todos los productos creados hasta ese momento
+
+// Debe contar con un método “getProductById” el cual debe buscar en el arreglo el producto que coincida con el id
+// En caso de no coincidir ningún id, mostrar en consola un error “Not found”
+
+// Formato del entregable
+
+// Pull Request (PR) de rama sprint-1 hacia main/master según corresponda
+// Incluir readme.md explicando lo que se entregó
 
 
-class TicketManager {
 
-    #gain
+class ProductManager {
+
+    #iva
 
     constructor() {
-        this.events = []
-        this.#gain = 0.15
+        this.products = []
+        this.#iva = 0.21
     }
 
-    getEvents() {
-        console.log(this.events)
-        return this.events
+    getProducts() {
+        console.log(this.products)
+        return this.products
     }
 
-    getEventById(event_id) {
-        let one = this.events.find(each=> each.id === event_id)
-        if (one) {
-            console.log(one)
-            return one
+    getProductById(product_id) {
+        let buscar = this.products.find(each=> each.id === product_id)
+        if (buscar) {
+            console.log(buscar)
+            return buscar
         }
         console.log('not found')
         return null
     }
 
-    addEvent({ name,place,price,capacity,date }) {
-        capacity = capacity ?? 50
-        date = date ?? new Date()
+    addProduct({title, description, price, thumbnail, stock}) {
         let id = 0
-        if (this.events.length===0) {
+        if (this.products.length===0){
             id = 1
         } else {
-            let lastEvent = this.events[this.events.length-1] //el ultimo elemento tiene indice igual a la longitud del array MENOS 1
-            id = lastEvent.id + 1
+            let lastProduct = this.products[this.products.length-1]
+            id = lastProduct.id + 1
         }
-        price = price + this.#gain * price
-        let event = { name,place,price,capacity,date,id, participants: [] }
-        //event.participants = []
-        this.events.push(event)
+        price = price + this.#iva * price
+        let product = {id,title, description, price, thumbnail, stock}
+        this.products.push(product)
     }
-
-    addParticipant(event_id,user_id) {
-        let found_event = this.getEventById(event_id)
-        //console.log(found_event.participants)
-        if (found_event) {
-            if (found_event.capacity > found_event.participants.length) {
-                let user = found_event.participants.includes(user_id)
-                if (user) {
-                    console.log(`el usuario ${user_id} ya se encuentra en la lista`)
-                } else {
-                    console.log('agregado usuario '+user_id)
-                    found_event.participants.push(user_id)
-                    //verificar que el array this.events fue o no fue modificado
-                    //en caso de no modificarse: usar un for para cambiar el evento con la modificacion realizada
-                    //console.log(found_event.participants)
-                    //return user_id        //lo sacamos de la condicion ya que es necesario seguir con la condicion del cambio d enombre
-                }
-            } else {
-                console.log('no hay mas capacidad')
-            }
-        }/*  else {
-            console.log('no se encontró el evento '+event_id);
-        } */
-        return null
-    }
-
-    changeName(event_id,new_name) {
-        let found_event = this.getEventById(event_id)
-        found_event.name = new_name
-            /*
-            for (let each of this.events) {
-                if (each.id === event_id) { //si coincide el id del evento a modificar
-                    return found_event      //retorno el evento encontrado y luego modificado
-                } else {                    //en caso contrario (no coincide)
-                    return each             //retorno el evento del array
-                }
-            }
-            */
-        console.log('nombre cambiado')
-    }
-
-    editEvent(event_id,datos) {  //datos va a ser el objeto con las variables a modificar (puede ser solo name o puede name con place o pueden ser TODOS los campos)
-        let found_event = this.getEventById(event_id)
-        for (let propiedad in datos) {
-            //console.log(propiedad)
-            found_event[propiedad] = datos[propiedad]
-        }
-    }
-
-    addNewEvent(event_id,new_place,new_date) {
-        let found_event = { ...this.getEventById(event_id) }
-        this.addEvent({
-            name: found_event.name,
-            place: new_place,
-            price: found_event.price,
-            capacity: found_event.capacity,
-            date: new_date
-        })
-        console.log('se creo el nuevo evento')
-    }
-
-    deleteEvent(event_id) {
-        this.events = this.events.filter(each => each.id !== event_id)
-        console.log('evento eliminado')
-    }
-
 }
 
-let ticket = new TicketManager()
-//console.log(ticket.gain)
-ticket.addEvent({ name: 'alice in borderland', place: 'korea', price: 5,capacity: null, date: undefined })
-ticket.addEvent({ name: 'hp', place: 'england', price: 10 })
-ticket.addEvent({ name: 'pokemon', place: 'japon', price: 50,capacity: 2, date: new Date('07/09/2023') })
-ticket.addEvent({ name: 'disney', place: 'miami', price: 100,capacity: 5000, date: new Date('07/20/2023') })
-//ticket.getEvents()
-//ticket.getEventById(2)
-//ticket.getEventById(9)
-//ticket.addParticipant(3,5)
-//ticket.addParticipant(3,9)
-//ticket.addParticipant(3,15)
-//ticket.addParticipant(1,5)
-//ticket.addParticipant(1,5)
-//ticket.addParticipant(1,5,'hola')
-//ticket.getEventById(1)
-//ticket.addNewEvent(3,'china',new Date('08/20/2023'))
-//ticket.getEvents()
-//ticket.deleteEvent(1)
-//ticket.getEvents()
-ticket.editEvent(1,{name: 'hola', place: 'rosario'})
-ticket.getEventById(1)
+let product = new ProductManager()
+
+product.addProduct({title: 'iphone x', description: 'Usadito pero anda bien :)', price: 35000, thumbnail: './img/iphone002.jpg', stock: 3})
+product.addProduct({title: 'PC Gamer I7', description: 'Nave de pc', price: 80000, thumbnail: './img/pc002.jpg', stock: 20})
+product.addProduct({title: 'Ipad pro retina', description: 'Nuevita ', price: 90, thumbnail: './img/ipad998.jpg', stock: 15})
+product.addProduct({title: 'Calefon electrico', description: 'hay que facturar asi que vendemos lo que sea', price: 2000, thumbnail: './img/calefon.jpg', stock: 10})
+product.addProduct({title: 'HeadSet Astros A50', description: 'Una locura lo que se escuchan', price: 5064, thumbnail: './img/a50.jpg', stock: 25})
+product.addProduct({title: 'Camara Canon EOS T6', description: 'Hermosa camara, sentite un profesional', price: 60000, thumbnail: './img/canont6.jpg', stock: 1})
+
+product.getProducts()
+product.getProductById(5)
+product.getProductById(10)
